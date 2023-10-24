eb03c2ca18c71df705068dd86483a434ba32c57e
jaquellsensi <146892715+jaquellsensi@users.noreply.github.com>
2023-10-24 13:57:18 -0700

    setup

---
+++ functions/simple_functions.js
@@ -0,0 +1,13 @@
+function favoriteThngs() {
+    console.log("My name is Jaquell");
+    console.log("My favorite movie is The Lion King");
+    console.log("My favorite band is Peso Pluma");
+}
+
+function whyImHere() {
+    console.log("I'm here to learn how to code");
+}
+
+function favoritePlace() {
+    console.log("My favorite place is North Carolina");
+}
